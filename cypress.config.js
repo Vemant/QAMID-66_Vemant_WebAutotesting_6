07597f1efd6f17cc41586133b821311519c0d4e8
unknown <vemant81@gmail.com>
2024-04-29 12:58:57 +0300

    WebAuto_6.3

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     retries: 2,
+    viewportWidth: 1000,
+    viewportHeight: 600,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
